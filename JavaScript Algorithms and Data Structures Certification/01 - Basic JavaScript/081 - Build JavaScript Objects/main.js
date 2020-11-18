1be5a2da2712b74ef02d8c7c6dcbf33f8923dc57
QjStout <qjstoutspykers@gmail.com>
2020-11-19 00:25:22 +1100

    Caught up on Day 10

---
+++ JavaScript Algorithms and Data Structures Certification/01 - Basic JavaScript/081 - Build JavaScript Objects/main.js
@@ -0,0 +1,9 @@
+var myDog = {
+  // Only change code below this line
+  'name': 'Scruffy',
+  'legs': 3,
+  'tails': 0.5,
+  'friends': ['Kitty', 'Birdy']  
+  // Only change code above this line
+  };
+  
