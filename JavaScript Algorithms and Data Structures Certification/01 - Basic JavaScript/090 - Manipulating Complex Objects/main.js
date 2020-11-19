b5496d692e39f1e9d592cbfdfa08a71000d21630
QjStout <qjstoutspykers@gmail.com>
2020-11-19 23:24:17 +1100

    Day 11

---
+++ JavaScript Algorithms and Data Structures Certification/01 - Basic JavaScript/090 - Manipulating Complex Objects/main.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Tool",
+    "title": "Lateralus",
+    "release_year": 2001,
+    "formats": [
+      "CD",
+      "2LP",
+      "Digital"
+    ],
+    "gold": true
+  }
+];
