45231ec6a268ad795a2f7af23aff976ad2ab1606
QjStout <qjstoutspykers@gmail.com>
2020-12-03 16:48:07 +1100

    Day 21

--- JavaScript Algorithms and Data Structures Certification/03 - Introduction to the Regular Expression Challenges/010 - Match Numbers and Letters of the Alphabet/main.js
+++ JavaScript Algorithms and Data Structures Certification/03 - Introduction to the Regular Expression Challenges/010 - Match Numbers and Letters of the Alphabet/main.js
@@ -1,3 +1,3 @@
 let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /change/; // Change this line
-let result = myRegex; // Change this line
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
