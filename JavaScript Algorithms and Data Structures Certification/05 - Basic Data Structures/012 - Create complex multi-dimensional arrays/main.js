b7671cc5b66b7a98bd693925271db6acaeb4a18f
QjStout <qjstoutspykers@gmail.com>
2021-01-13 05:42:31 +1100

    Day 29

---
+++ JavaScript Algorithms and Data Structures Certification/05 - Basic Data Structures/012 - Create complex multi-dimensional arrays/main.js
@@ -0,0 +1,18 @@
+let myNestedArray = [
+  // Only change code below this line
+  'one',
+  [
+    'two',
+    [
+      'deep',
+      [
+        'deeper',
+        [
+          'deepest'
+        ]
+      ]
+    ]
+  ]
+
+  // Only change code above this line
+];
