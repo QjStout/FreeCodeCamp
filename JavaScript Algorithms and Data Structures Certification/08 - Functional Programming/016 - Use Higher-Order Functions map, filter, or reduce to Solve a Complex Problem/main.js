e083030f74ac8669209173072ca6cd9a10f2b024
QjStout <qjstoutspykers@gmail.com>
2021-01-30 04:09:34 +1100

    Day 42

---
+++ JavaScript Algorithms and Data Structures Certification/08 - Functional Programming/016 - Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem/main.js
@@ -0,0 +1,10 @@
+const squareList = arr => {
+  // Only change code below this line
+  return arr
+    .filter(num => Number.isInteger(num) && num > 0)
+    .map(num => num**2);
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
